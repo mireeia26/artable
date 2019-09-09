589267941103e050b9fe13b8dc52497f43ad31d8
Mireia <mireia.perez@mango.com>
2019-09-09 09:34:17 +0200

    Initial commit

---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by MIREIA PEREZ BARROS on 09/09/2019.
+//  Copyright © 2019 mpb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
